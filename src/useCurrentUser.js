2c023e4320d7d1ab45fbafad125bec902d6884d5
nickgarfield <ntgarfield@gmail.com>
2020-12-01 22:31:12 -0600

    🧹 CLEAN: Remove unneeded log statements

--- src/useCurrentUser.js
+++ src/useCurrentUser.js
@@ -1,5 +1,4 @@
 import { useEffect, useState } from 'react'
-// const React = require('react')
 
 function useCurrentUser() {
     const [authState, setAuthState] = useState({
--- src/useRedirectHandler.js
+++ src/useRedirectHandler.js
@@ -1,22 +1,12 @@
 import { useEffect } from 'react'
-// const React = require('react')
 import { baseUrl, clientId, redirectUrl } from './configure'
 
-console.log('A')
-
 function useRedirectHandler(callback) {
-    console.log('B')
-
     useEffect(() => {
-
-        console.log('C')
-
         const urlParams = new URLSearchParams(window.location.search)
         const state = urlParams.get('state')
         const code = urlParams.get('code')
         const grantType = 'authorization_code'
-        
-        console.log('D')
 
         // TODO verify state
 
@@ -24,27 +14,17 @@ function useRedirectHandler(callback) {
             .then(res => res.json())
             .then(tokens => {
 
-                console.log('E')
-
                 // Save tokens to localstorage and cookies
                 const cookies = require('cookie-cutter')
                 cookies.set('auth-idtoken', tokens.id_token)
                 localStorage.setItem('auth-session', JSON.stringify(tokens))
-
-                console.log('F')
-
                 if (callback) callback()
-
-                console.log('G')
             })
             .catch(err => {
-
-                console.log('H')
-
+                
+                // TODO handle error
                 console.log(err)
             })
-
-            console.log('I')
     }, [])
 }
 
