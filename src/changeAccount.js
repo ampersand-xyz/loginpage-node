4695b3274456ccb064083c6772f0fb225dac36d2
nickgarfield <ntgarfield@gmail.com>
2020-12-10 17:58:12 -0600

    🐜 FIX: Parse json reponse from fetch

--- src/changeAccount.js
+++ src/changeAccount.js
@@ -8,11 +8,12 @@ async function changeAccount(accountId) {
         console.log('B')
         console.log(authSession)
         const grantType = 'bearer_token'
-        const tokens = await fetch(`${protocol}www.${host}/api/token?grant_type=${grantType}&account_id=${accountId}`, {
+        const res = await fetch(`${protocol}www.${host}/api/token?grant_type=${grantType}&account_id=${accountId}`, {
             headers: {
                 'Authorization': 'Bearer ' + authSession.id_token,
             }
         })
+        const tokens = await res.json()
         console.log('C')
         console.log(tokens)
 
