7a7d33df626990edcb3a539d1bed79637332c1fa
nickgarfield <ntgarfield@gmail.com>
2020-12-10 17:52:38 -0600

    🐜 FIX: Missing await call

--- src/changeAccount.js
+++ src/changeAccount.js
@@ -5,7 +5,7 @@ async function changeAccount(accountId) {
     try {
         const authSession = JSON.parse(localStorage.getItem('auth-session'))
         const grantType = 'bearer_token'
-        const tokens = fetch(`${protocol}www.${host}/api/token?grant_type=${grantType}&account_id=${accountId}`, {
+        const tokens = await fetch(`${protocol}www.${host}/api/token?grant_type=${grantType}&account_id=${accountId}`, {
             headers: {
                 'Authorization': 'Bearer ' + authSession.id_token,
             }
@@ -15,7 +15,6 @@ async function changeAccount(accountId) {
         const cookies = require('cookie-cutter')
         cookies.set('auth-idtoken', tokens.id_token)
         localStorage.setItem('auth-session', JSON.stringify(tokens))
-
         return tokens.id_token
 
     } catch (e) {
