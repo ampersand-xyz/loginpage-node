d0f1dd3b65bca3bd32ebd146bdbb49602fc8d17a
nickgarfield <ntgarfield@gmail.com>
2021-01-14 11:05:49 -0600

    📦 Update signIn to check for custom domains

--- src/signIn.js
+++ src/signIn.js
@@ -1,8 +1,24 @@
 import { protocol, host, appId, clientId, redirectUrl } from './configure'
 
-function signIn() {
+async function signIn() {
     const state = 'abc' // TODO randomize
-    window.location.replace(`${protocol}${appId}.${host}/m?clientId=${clientId}&redirectUri=${redirectUrl}&state=${state}`)
+    
+    // TODO Fetch custom domain for app id
+    var customDomain = null
+    try {
+        var domainRes = await fetch(`https://www.login.page/api/domains?app_id=${appId}`)
+        domainRes = await domainRes.json()
+        customDomain = domainRes.domain
+    } catch (e) {
+        // Do nothing
+    }
+    
+    // Redirect to the custom domain
+    if (customDomain) {
+        window.location.replace(`${protocol}${customDomain}/m?clientId=${clientId}&redirectUri=${redirectUrl}&state=${state}`)
+    } else {
+        window.location.replace(`${protocol}${appId}.${host}/m?clientId=${clientId}&redirectUri=${redirectUrl}&state=${state}`)
+    }
 }
 
 export default signIn
